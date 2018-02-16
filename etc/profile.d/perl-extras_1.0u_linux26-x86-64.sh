97275bb6c2f84e6dd4ffa61310ba956d37f2e208
Michel Valin <mfvalin@gmail.com>
2018-02-16 17:05:03 -0500

    needed perl modules that might be missing locally

---
+++ etc/profile.d/perl-extras_1.0u_linux26-x86-64.sh
@@ -0,0 +1,2 @@
+[[ -n /home/valin/ssm-domains-base/setup/env_003/perl-extras_1.0u_linux26-x86-64/lib/perl5/site_perl ]] && export PERL5LIB=/home/valin/ssm-domains-base/setup/env_003/perl-extras_1.0u_linux26-x86-64/lib/perl5/site_perl:/home/valin/ssm-domains-base/setup/env_003/perl-extras_1.0u_linux26-x86-64/lib/perl5/site_perl
+[[ -z /home/valin/ssm-domains-base/setup/env_003/perl-extras_1.0u_linux26-x86-64/lib/perl5/site_perl ]] && export PERL5LIB=/home/valin/ssm-domains-base/setup/env_003/perl-extras_1.0u_linux26-x86-64/lib/perl5/site_perl
